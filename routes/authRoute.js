31a04680ccad0dc13b5fd0f9d3307b0ca616b31f
Deepak Bhangale <deepakbhangale1996@gmail.com>
2022-05-10 11:59:37 +0530

    Added swagger

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,13 +5,68 @@ const mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const auth = require("./../middleware/auth");
+var bodyParser = require('body-parser');
 
+
+router.use(bodyParser.json()); // to use body object in requests
+/**
+ * @swagger
+ *  tags:
+ *    name: Users
+ *    description: List of users
+ */
+
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Returns all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: the list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/auths'
+ */
 router.route("/").get((req, res) => {
   res.send("Welcome to Node JS App")
 })
 
+/**
+ * @swagger
+ * /:
+ *   post:
+ *     summary: Create a new User
+ *     tags: [Users]
+ *     consumes:
+ *        - application/json
+ *     requestBody:
+ *       required: true
+ *     content:
+ *          application/json:
+ *     parameters: 
+ *        - in: body
+ *          schema:
+ *              $ref: '#/components/schemas/auths'
+ *              type: object
+ *     responses:
+ *       200:
+ *         description: The User was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/auths'
+ *       500:
+ *         description: Some server error
+ */
 router.route("/").post((req, res) => {
     const { name, description,email,password } = req.body;
+    console.log(req.body)
     AuthModel.find({ email: email }).then((res1) => {
         if (res1) {
             const newUser = AuthModel({ name, description,email,password })
@@ -20,6 +75,7 @@ router.route("/").post((req, res) => {
                     return res.status(200).json(user)
                 })
                 .catch((error) => {
+                    console.log(error)
                     return res.status(500).json({ "Error": 'User Already Exist with this email' })
                 })
 
@@ -30,6 +86,22 @@ router.route("/").post((req, res) => {
         return res.status(500).json({ "Error": err })
     })
 })
+/**
+ * @swagger
+ * /all:
+ *   get:
+ *     summary: Returns all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: the list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/auths'
+ */
 router.route("/all").get(paginatedData(AuthModel), (req, res) => {
     AuthModel.find().then((res1) => {
         if (res1) {
@@ -42,6 +114,23 @@ router.route("/all").get(paginatedData(AuthModel), (req, res) => {
         return res.status(500).json({ "Error": err })
     })
 })
+
+/**
+ * @swagger
+ * /user/:id:
+ *   patch:
+ *     summary: Returns all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: the list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/auths'
+ */
 router.route("/user/:id").patch((req, res) => {
     const { name, description} = req.body;
     AuthModel.findByIdAndUpdate(req.params.id, { description: description,name:name }, { new: true }, function (err, result) {
@@ -55,7 +144,35 @@ router.route("/user/:id").patch((req, res) => {
     })
 
 })
+/**
+ * @swagger
+ * /user/{id}:
+ *   delete:
+ *     summary: Returns all users
+ *     tags: [Users]
+ *     consumes:
+ *        - application/json
+ *     "parameters": [
+ *               {
+ *                   "name": "id",
+ *                   "in": "path",
+ *                   "description": "The username of the user",
+ *                   "required": true,
+ *                   "type": "string"
+ *               }
+ *               ]
+ *     responses:
+ *       200:
+ *         description: the list of the users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/auths'
+ */
 router.route("/user/:id").delete((req, res) => {
+    console.log(req.params.id)
     AuthModel.findByIdAndDelete(req.params.id)
         .then((response) => {
             res.status(200).json('User Deleted');
--- server.js
+++ server.js
@@ -5,9 +5,49 @@ const cors = require('cors');
 const { API_PORT } = process.env;
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
+const swaggerJsonDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 
 require("dotenv").config();
 require("./config/database").connect();
+
+
+
+
+const swaggerOptions = {
+	swaggerDefinition: {
+		info: {
+			title: "Documenting REST API's",
+			description: "This is an implementation of how to document your RESTful API's using SWAGGER",
+			servers: ['http://localhost:4001']
+		},
+		"components": {
+			"schemas": {
+				"auths": {
+					"properties": {
+						"name": {
+							"type": "string"
+						},
+						"description": {
+							"type": "string"
+						},
+						"email": {
+							"type": "string"
+						},
+						"password": {
+							"type": "string"
+						}
+					}
+				}
+			}
+		}
+	},
+	apis: ['./routes/authRoute.js']
+}
+
+const swaggerDocs = swaggerJsonDoc(swaggerOptions)
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
+
 
 
 // bodyParser middleware
@@ -15,7 +55,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors());
 
-  
+
 // Routes
 const authRoute = require("./routes/authRoute");
 
@@ -38,12 +78,12 @@ app.use("/", authRoute);
 // if (process.env.NODE_ENV === "production") {
 //     // set a static folder
 //     app.use(express.static("client/build"));
-  
+
 //     // Provide a wildcard as a fallback for all routes
 //     app.get("*", (req, res) => {
 //       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 //     });
 //   }
-  
-  //Host app at PORT
-  app.listen(port, () => console.log(`Server is running at PORT ${port}!`));
+
+//Host app at PORT
+app.listen(port, () => console.log(`Server is running at PORT ${port}!`));
